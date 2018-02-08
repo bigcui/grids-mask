397f8f89b07ea5730cc818c112ebc055db0fa2a3
cuihonglei <cuihonglei@baidu.com>
2018-02-08 20:20:19 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # grids-mask
+按照commonJS语法规范，兼容node环境和webpack等构建工具c
 栅格化ui 校验器
+
+不支持`<script src='my-plugin/myPlugin.js'></script>`
+##使用如下##
+`import maskshange from 'grids-mask';
+maskhange();`
+
+在页面url增加'&shange=true'
---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * @file 栅格ui浮层
+ *
+ * @author  cuihonglei
+ * 2018年2月7日
+ */
+
+/* eslint-disable */
+
+
+ function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+    var search = window.location.href.split('?')[1];
+    if (search) {
+        var r = search.match(reg);
+        if (r !== null) {
+            return decodeURIComponent(r[2]);
+        }
+    }
+    return '';
+};
+module.exports = function() {
+    
+    let poi = [
+        ['position: fixed'],
+        ['top: 0'],
+        ['left: 0'],
+        ['z-index: 9999'],
+        ['right: 0'],
+        ['bottom: 0'],
+        [' background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABNoAAAACCAYAAACOqbEbAAAAY0lEQVRoge3RwQ1AQABEUUQVmnHmpDK1SCR0pAYHB03YjGzeK2AyyW/H9d6bShzDlr7we880Fd3vz7Pofg00yNMgT4M8DfI0yNMgT4M8DfI0yNMgb76W9IXPdOkDAAAAAFCDF7CfJnxdI10yAAAAAElFTkSuQmCC) repeat-y'],
+        [' -webkit-background-size: 100% 100%'],
+        [' background-size: 100%']
+    ];
+    let body = document.querySelector('body');
+    let mask = document.createElement('div');
+        mask.className = 'shange-mask';
+        
+    if(getQueryString('shange')){
+        body.appendChild(mask);
+        mask.style.cssText = poi.join(';');
+    }
+       
+};
